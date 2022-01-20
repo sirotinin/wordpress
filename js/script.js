c2b3caf5f43996053e8fed0f225ed736ff0b144a
Socrates <example@gmail.com>
2022-01-20 03:09:08 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert("Hello");
+
+//const results = confirm("Are you here?");
+//console.log(results);
+
+//const answer = prompt("How old are you?", "18");
+//console.log(answer + 349);
+
+const answer = [];
+answer[0] = prompt("How are you?", " ");
+answer[1] = prompt("How old are you?", " ");
+answer[2] = prompt("How match?", "");
+
+document.write(answer);
+
+let incr = 10,
+    decr = 10;
